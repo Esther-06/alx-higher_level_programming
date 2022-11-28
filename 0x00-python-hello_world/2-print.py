0a2d3cc4cab1a8f0bc6997d6a643b7556122f6d4
Esther-06 <wairimuw@gmail.com>
2022-11-28 08:18:25 -0800

    2nd challenge

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,10 +1,2 @@
 #!/bin/bash
 print("\"Programming is like building a multilingual puzzle")
-
-#!/bin/bash
-print("\"Programming is like building a multilingual puzzle")
-
-#!/bin/bash
-print("\"Programming is like building a multilingual puzzle")
-
-
