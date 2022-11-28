a7b952223717e5ca937fc1bf6a9718bbeb6198cb
Esther-06 <wairimuw@gmail.com>
2022-11-28 08:09:37 -0800

    python script

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+print("\"Programming is like building a multilingual puzzle")
+
+#!/bin/bash
+print("\"Programming is like building a multilingual puzzle")
+
+#!/bin/bash
+print("\"Programming is like building a multilingual puzzle")
+
+
