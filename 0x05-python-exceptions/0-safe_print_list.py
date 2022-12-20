665afad7d4c07c0fdf4c535a026044e9a5cf0845
Esther-06 <wairimuw@gmail.com>
2022-12-19 17:42:33 -0800

    9th cha

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
         n = 0           
         for m in range(x):
          try:
-            print(my_list[m], end="")
-            n += 1
-         except IndexError:
+            print("{}".format(my_list[m], end="")
+        except IndexError:
              break
-      print()
+        n += 1
+      print("")
       return n
