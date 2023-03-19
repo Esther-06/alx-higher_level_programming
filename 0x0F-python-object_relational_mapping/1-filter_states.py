26e065cce111a78de141b676c7316a3a04a2790b
Esther-06 <wairimuw@gmail.com>
2023-03-19 03:07:34 -0700

    fix code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,18 @@ parameters given to script: sername, passwaord, database
 import MySQLdb
 from sys import argv
 
-if__name__ == '__main__':
+if__name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     #connect to database
     db= MySQLdb.connect(host="localhost",
                         port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+                        user=username,
+                        passwd=password,
+                        db=db_name)
 
     #create cursor to exec queries using SQL; filter names starting with 'N'
     cursor = db.cursor()
