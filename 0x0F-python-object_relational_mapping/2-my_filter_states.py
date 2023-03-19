35369baf9d18b7d256f2c9379942033f6d549ac8
Esther-06 <wairimuw@gmail.com>
2023-03-19 02:48:35 -0700

    matches args

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,23 @@ parameters given to script: username, password, database, state to match
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":)
-     # connect to database
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
     # create cursor to exec queries using SQL; match arg given
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    cursor = db.cursor()
+    sql_cmd = """SELECT *
+                 FROM states
+                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
+    cursor.execute(sql_cmd)
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
     cursor.close()
     db.close()
